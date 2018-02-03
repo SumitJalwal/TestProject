715c8e1710d5098f92e944cbca57b962a441c88c
sumit <sumitdeep93@gmail.com>
2018-02-03 23:26:20 +0530

    new change

--- src/HelloWorldAction.java
+++ src/HelloWorldAction.java
@@ -3,8 +3,8 @@ package org.hello;
 public class HelloWorldAction {
 
 	public String execute(){
-		System.out.println("hello from sumit and bhavna");
-		return "success";
+		System.out.println("hello from sumit and bhavna this is some change");
+		return "succesx";
 	}
 	
 }
